0d3a7589fe3a6e97c58735c8339035a96aa5f70e
Maverobot <quzhengrobot@gmail.com>
2018-12-26 00:08:30 +0100

    added deng zi qi for testing

--- facenet.py
+++ facenet.py
@@ -157,6 +157,7 @@ database["kevin"] = img_to_encoding("images/kevin.jpg", FRmodel)
 database["felix"] = img_to_encoding("images/felix.jpg", FRmodel)
 database["benoit"] = img_to_encoding("images/benoit.jpg", FRmodel)
 database["arnaud"] = img_to_encoding("images/arnaud.jpg", FRmodel)
+database["deng"] = img_to_encoding("images/deng.jpg", FRmodel)
 
 print("Some tests...")
 verify("images/camera_0.jpg", "younes", database, FRmodel)
