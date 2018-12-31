492a324f259b3033a2354fd398e29bc01404aa47
Maverobot <quzhengrobot@gmail.com>
2018-12-31 11:35:17 +0100

    add more options for facenet client

--- facenet_client.py
+++ facenet_client.py
@@ -2,10 +2,17 @@
 
 import xmlrpc.client
 import argparse
-parser = argparse.ArgumentParser("client.py")
-parser.add_argument("image_path", help="The path to the face image.", type=str)
+parser = argparse.ArgumentParser('client.py')
+parser.add_argument('image_path', help="The path to the face image.", type=str)
+parser.add_argument('-v', '--verify', dest='name', help="Verify if the image has the face of the given name.", type=str)
 args = parser.parse_args()
-print(args.image_path)
+try:
+    s = xmlrpc.client.ServerProxy("http://localhost:8000")
+    if args.name:
+        print(s.verify(args.image_path, args.name))
+    else:
+        print(s.get_identity(args.image_path))
+except ConnectionRefusedError as e:
+    print(e)
+    print("The connection to facenet server has failed.")
 
-s = xmlrpc.client.ServerProxy('http://localhost:8000')
-print(s.get_identity(args.image_path))
